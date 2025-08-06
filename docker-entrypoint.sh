0485b3c49e72eb68e0c49167299811788b2310a1
Alstemical <stemgardeninfinity@proton.me>
2025-08-06 16:26:09 -0400

    Add settings for RQ worker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,5 @@
 #!/bin/sh
 
-set -e
+flask db upgrade
 
-echo "Waiting for PostgreSQL..."
-
-while ! nc -z dpg-d294dher433s73c72qs0-a 5432; do
-  sleep 0.1
-done
-
-echo "PostgreSQL is up! Running migrations and starting server."
-
-
-
-flask db upgrade --directory migrations
-
-# Start RQ worker in the background
-rq worker &
-
-# Start Gunicorn web server in the foreground
 exec gunicorn --bind 0.0.0.0:80 "app:create_app()"
