0e1d647318f679eccd1203bfa5c433e6bd01316f
Alstemical <stemgardeninfinity@proton.me>
2025-08-06 12:43:14 -0400

    Fix NameError in Mailgun task worker

--- tasks.py
+++ tasks.py
@@ -9,9 +9,9 @@ DOMAIN = os.getenv("MAILGUN_DOMAIN")
 
 def send_simple_message(to, subject, body):
     return requests.post(
-        f"https://api.mailgun.net/v3/{domain}/messages",
+        f"https://api.mailgun.net/v3/{DOMAIN}/messages",
         auth=("api", os.getenv("MAILGUN_API_KEY")),
-        data={"from": f"T-Stem <mailgun@{domain}>",
+        data={"from": f"T-Stem <mailgun@{DOMAIN}>",
               "to": [to],
               "subject": subject,
               "text": body})
